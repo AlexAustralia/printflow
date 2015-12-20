e502c04b9db7488fd1d975954e843d6981cb2eda
Alex Sobolev <alexaustrali125@gmail.com>
2015-12-20 19:54:02 +0500

    Fixed Searching suppliers

--- resources/views/suppliers/index.blade.php
+++ resources/views/suppliers/index.blade.php
@@ -1,65 +1,67 @@
 @extends('app')
 
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
-
-<style type="text/css" media="screen">
-
-    #suppliers ul { margin: 0; padding: 0; overfow: hidden; clear: both;}
-    #suppliers li { list-style: none; }
-
-    #suppliers input.search { 
-        border: 1px solid #aaa;
-        float: right;
-        font-size: 15px;
-        margin-right: -15px;
-        /*margin-top: -50px;*/
-        padding: 5px;        
-    }
-
-    #suppliers .supplier-row {
-        border: 1px solid #f2f2f2;
-        margin-bottom: 20px;
-    }
-
-    #suppliers .supplier {
-        background: #F9F9F9;
-        padding: 10px;
-        font-weight: bold;
-        border-bottom: 1px solid #eee;
-    }
-
-    #suppliers .supplier .btn.edit {
-        margin: -4px;
-    }
-
-    #suppliers .contacts .heading {
-        width: 100%;
-        clear: both;
-        color: #aaa;
-        padding: 15px;
-        font-family: Helvetica, Arial;
-    }
-    
-    #suppliers .contacts .fa {
-        margin-right: 10px;
-    }
-
-    #suppliers .contact {
-        overflow: hidden;
-    }
-
-    #suppliers .contact:last-child {
-        padding-bottom: 20px;
-    }
-
-    #suppliers .unavailable {
-        color: #aaa;
-    }
-
-</style>
-
 @section('title')
 Suppliers
+@endsection
+
+@section('styles')
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
+
+    <style type="text/css" media="screen">
+
+        #suppliers ul { margin: 0; padding: 0; overfow: hidden; clear: both;}
+        #suppliers li { list-style: none; }
+
+        #suppliers input.search {
+            border: 1px solid #aaa;
+            float: right;
+            font-size: 15px;
+            margin-right: -15px;
+            /*margin-top: -50px;*/
+            padding: 5px;
+        }
+
+        #suppliers .supplier-row {
+            border: 1px solid #f2f2f2;
+            margin-bottom: 20px;
+        }
+
+        #suppliers .supplier {
+            background: #F9F9F9;
+            padding: 10px;
+            font-weight: bold;
+            border-bottom: 1px solid #eee;
+        }
+
+        #suppliers .supplier .btn.edit {
+            margin: -4px;
+        }
+
+        #suppliers .contacts .heading {
+            width: 100%;
+            clear: both;
+            color: #aaa;
+            padding: 15px;
+            font-family: Helvetica, Arial;
+        }
+
+        #suppliers .contacts .fa {
+            margin-right: 10px;
+        }
+
+        #suppliers .contact {
+            overflow: hidden;
+        }
+
+        #suppliers .contact:last-child {
+            padding-bottom: 20px;
+        }
+
+        #suppliers .unavailable {
+            color: #aaa;
+        }
+
+    </style>
 @endsection
 
 @section('content')
@@ -86,8 +88,8 @@ Suppliers
 
     <li class="row supplier-row">
 
-        <div class="supplier">
-            {!! $c->supplier_name !!} 
+        <div>
+            <span  class="supplier">{!! $c->supplier_name !!}</span>
             <div class="pull-right"> 
                 <a class="btn btn-default edit" title="Edit {!! ucwords(strtolower($c->supplier_name)) !!}" href="{!! route('suppliers.edit', ['id' => $c->id]) !!}"><i class="fa fa-pencil"></i></a> 
             </div>
