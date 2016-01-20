9d233572b10727ccdd30991154e894692a16133e
Alex Sobolev <alexaustrali125@gmail.com>
2016-01-21 00:37:31 +0500

    Fixed issues with sending emails to suppliers

--- app/Supplier.php
+++ app/Supplier.php
@@ -41,6 +41,8 @@ class Supplier extends Model {
     public function primary_person(){
         $contact = $this->supplier_contacts()->where('primary_person', '=', '1')->first();
 
+        if($contact != null) return $contact;
+
         if ($contact == null && count($this->supplier_contacts()) > 0){
             //die("No primary contact for ".$this->supplier_name.", returning first contact");
             return $this->supplier_contacts()->first();
